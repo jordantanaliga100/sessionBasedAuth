5a5aebf4eacf3b499ee5dee3c00bf32e2eadcece
jordantanaliga100 <jordantanaliga100@gmail.com>
2026-02-08 14:48:59 +0800

    chore: create client | shifts to docker, serve with nginx

---
+++ sba-redis/server/src/db/redis/redis.config.ts
@@ -0,0 +1,30 @@
+import { RedisStore } from 'connect-redis'
+import session from 'express-session'
+import { createClient } from 'redis'
+
+// 1. Initialize Redis Client agad
+const redisClient = createClient({
+    url: `redis://${process.env.REDIS_HOST || 'redis'}:${process.env.REDIS_PORT || 6379}`,
+})
+
+// Connect at handle error
+redisClient.connect().catch(console.error)
+
+// 2. I-setup ang Store
+const redisStore = new RedisStore({
+    client: redisClient,
+    prefix: 'sess:',
+})
+
+// 3. I-export ang session middleware
+export const sessionConfig = session({
+    store: redisStore, // Force Redis Store
+    secret: process.env.SESSION_SECRET || 'pogi-secret-key',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 1000 * 60 * 60 * 24, // 1 day
+    },
+})
